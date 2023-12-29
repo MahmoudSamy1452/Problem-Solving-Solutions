705c50ddfaa19b1e3bce29ffa5632fca052df9fc
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2023-12-29 14:53:32 +0200

    29.12.23 Daily Solve

---
+++ 1335. Minimum Difficulty of a Job Schedule.cpp
@@ -0,0 +1,61 @@
+// 1335. Minimum Difficulty of a Job Schedule
+
+// You want to schedule a list of jobs in d days. Jobs are dependent (i.e To work on the ith job, you have to finish all the jobs j where 0 <= j < i).
+
+// You have to finish at least one task every day. The difficulty of a job schedule is the sum of difficulties of each day of the d days. The difficulty of a day is the maximum difficulty of a job done on that day.
+
+// You are given an integer array jobDifficulty and an integer d. The difficulty of the ith job is jobDifficulty[i].
+
+// Return the minimum difficulty of a job schedule. If you cannot find a schedule for the jobs return -1.
+// Example 1:
+
+// Input: jobDifficulty = [6,5,4,3,2,1], d = 2
+// Output: 7
+// Explanation: First day you can finish the first 5 jobs, total difficulty = 6.
+// Second day you can finish the last job, total difficulty = 1.
+// The difficulty of the schedule = 6 + 1 = 7
+// Example 2:
+
+// Input: jobDifficulty = [9,9,9], d = 4
+// Output: -1
+// Explanation: If you finish a job per day you will still have a free day. you cannot find a schedule for the given jobs.
+// Example 3:
+
+// Input: jobDifficulty = [1,1,1], d = 3
+// Output: 3
+// Explanation: The schedule is one job per day. total difficulty will be 3.
+
+// Constraints:
+
+// 1 <= jobDifficulty.length <= 300
+// 0 <= jobDifficulty[i] <= 1000
+// 1 <= d <= 10
+
+class Solution
+{
+public:
+  int minDifficulty(vector<int> &jobDifficulty, int d)
+  {
+    vector<vector<int>> dp(d + 1, vector<int>(jobDifficulty.size() + 1, 0));
+    int mx = INT_MIN;
+    for (int i = jobDifficulty.size() - 1; i >= 0; i--)
+    {
+      mx = max(mx, jobDifficulty[i]);
+      dp[1][i] = mx;
+    }
+    for (int i = 2; i <= d; i++)
+    {
+      for (int j = jobDifficulty.size() - 1; j >= 0; j--)
+      {
+        dp[i][j] = INT_MAX;
+        mx = INT_MIN;
+        for (int k = j; k < jobDifficulty.size() && jobDifficulty.size() - k >= i; k++)
+        {
+          mx = max(mx, jobDifficulty[k]);
+          dp[i][j] = min(dp[i][j], mx + dp[i - 1][k + 1]);
+        }
+      }
+    }
+    return dp[d][0] == INT_MAX ? -1 : dp[d][0];
+  }
+};
