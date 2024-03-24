45b7b0a8d05c98033c5ccdbfeb331251ee12bdfa
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-03-24 12:47:25 +0200

    24.3.24 Daily Solve

---
+++ Leetcode/Daily Problems/287. Find the Duplicate Number.cpp
@@ -0,0 +1,56 @@
+287. Find the Duplicate Number
+
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+Input: nums = [3,3,3,3,3]
+Output: 3
+ 
+Constraints:
+
+1 <= n <= 1e5
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+
+Follow up:
+
+How can we prove that at least one duplicate number must exist in nums?
+Can you solve the problem in linear runtime complexity?
+
+class Solution
+{
+public:
+  int findDuplicate(vector<int> &nums)
+  {
+    int tortoise = 0, hare = 0;
+    while (true)
+    {
+      hare = nums[nums[hare]];
+      tortoise = nums[tortoise];
+      if (hare == tortoise)
+        break;
+    }
+
+    tortoise = 0;
+    while (hare != tortoise)
+    {
+      hare = nums[hare];
+      tortoise = nums[tortoise];
+    }
+    return hare;
+  }
+};
