cc4e3b17d9c8ed3a5004b4a37b28d7a662edeb4f
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-04-18 04:26:12 +0200

    17.4.24 Daily Solve

---
+++ Leetcode/Daily Problems/988. Smallest String Starting From Leaf.cpp
@@ -0,0 +1,65 @@
+// 988. Smallest String Starting From Leaf
+
+// You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters 'a' to 'z'.
+
+// Return the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+
+// As a reminder, any shorter prefix of a string is lexicographically smaller.
+
+// For example, "ab" is lexicographically smaller than "aba".
+// A leaf of a node is a node that has no children.
+
+// Example 1:
+
+// Input: root = [0,1,2,3,4,3,4]
+// Output: "dba"
+// Example 2:
+
+// Input: root = [25,1,3,1,3,0,2]
+// Output: "adz"
+// Example 3:
+
+// Input: root = [2,2,1,null,1,0,null,0]
+// Output: "abc"
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 8500].
+// 0 <= Node.val <= 25
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+  string smallestString;
+
+public:
+  string smallestFromLeaf(TreeNode *root)
+  {
+    dfs(root, "");
+    return smallestString;
+  }
+
+  void dfs(TreeNode *root, string currentString)
+  {
+    if (!root)
+      return;
+    currentString = char(root->val + 'a') + currentString;
+
+    if (!root->left && !root->right)
+      if (smallestString == "" || smallestString > currentString)
+        smallestString = currentString;
+
+    dfs(root->left, currentString);
+    dfs(root->right, currentString);
+  }
+};
