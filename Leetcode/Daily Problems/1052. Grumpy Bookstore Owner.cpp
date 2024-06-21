53f4c618a6800f6b1d0d529b2ca725234e63326a
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-06-21 14:14:21 +0300

    21.6.24 Daily Solve

---
+++ Leetcode/Daily Problems/1052. Grumpy Bookstore Owner.cpp
@@ -0,0 +1,54 @@
+// 1052. Grumpy Bookstore Owner
+
+// There is a bookstore owner that has a store open for n minutes. Every minute, some number of customers enter the store. You are given an integer array customers of length n where customers[i] is the number of the customer that enters the store at the start of the ith minute and all those customers leave after the end of that minute.
+
+// On some minutes, the bookstore owner is grumpy. You are given a binary array grumpy where grumpy[i] is 1 if the bookstore owner is grumpy during the ith minute, and is 0 otherwise.
+
+// When the bookstore owner is grumpy, the customers of that minute are not satisfied, otherwise, they are satisfied.
+
+// The bookstore owner knows a secret technique to keep themselves not grumpy for minutes consecutive minutes, but can only use it once.
+
+// Return the maximum number of customers that can be satisfied throughout the day.
+
+// Example 1:
+
+// Input: customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], minutes = 3
+// Output: 16
+// Explanation: The bookstore owner keeps themselves not grumpy for the last 3 minutes.
+// The maximum number of customers that can be satisfied = 1 + 1 + 1 + 1 + 7 + 5 = 16.
+// Example 2:
+
+// Input: customers = [1], grumpy = [0], minutes = 1
+// Output: 1
+
+// Constraints:
+
+// n == customers.length == grumpy.length
+// 1 <= minutes <= n <= 2 * 104
+// 0 <= customers[i] <= 1000
+// grumpy[i] is either 0 or 1.
+
+class Solution
+{
+public:
+  int maxSatisfied(vector<int> &customers, vector<int> &grumpy, int minutes)
+  {
+    int i = 0, j = 0, n = customers.size(), sum = 0, mx = 0;
+    while (j < n)
+    {
+      if (j == i + minutes)
+      {
+        sum -= grumpy[i] ? customers[i] : 0;
+        i++;
+      }
+      sum += grumpy[j] ? customers[j] : 0;
+      j++;
+      mx = max(mx, sum);
+    }
+    for (int k = 0; k < n; k++)
+    {
+      mx += !grumpy[k] ? customers[k] : 0;
+    }
+    return mx;
+  }
+};
