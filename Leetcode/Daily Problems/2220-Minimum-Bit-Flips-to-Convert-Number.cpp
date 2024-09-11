28515775235f6318c98137e1cd4a0199127be0ff
Mahmoud Samy <86854027+MahmoudSamy1452@users.noreply.github.com>
2024-09-12 00:56:12 +0300

    [2220] [Time Beats: 100.00%] [Memory Beats: 99.69%] - LeetPush

---
+++ Leetcode/Daily Problems/2220-Minimum-Bit-Flips-to-Convert-Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int diff = start ^ goal;
+        int cnt = 0;
+        while(diff){
+            cnt += (diff & 1);
+            diff >>= 1;
+        }
+        return cnt;
+    }
+};
