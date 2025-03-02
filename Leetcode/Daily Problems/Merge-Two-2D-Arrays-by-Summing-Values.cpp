c46aa02aa3dd1f00f6ac043486e4a77e3e293db1
Mahmoud Samy <86854027+MahmoudSamy1452@users.noreply.github.com>
2025-03-02 15:24:16 +0200

    [2570] [Time Beats: 100.00%] [Memory Beats: 64.62%] - LeetPush

---
+++ Leetcode/Daily Problems/Merge-Two-2D-Arrays-by-Summing-Values.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        int n = nums1.size(), m = nums2.size(), i = 0, j = 0;
+        vector<vector<int>> ans;
+
+        while(i < n && j < m) {
+            if(nums1[i][0] < nums2[j][0])
+                ans.push_back(nums1[i++]);
+            else if(nums1[i][0] > nums2[j][0])
+                ans.push_back(nums2[j++]);
+            else ans.push_back({nums1[i][0], nums1[i++][1] + nums2[j++][1]});
+        }
+
+        while(i < n) ans.push_back(nums1[i++]);
+        while(j < m) ans.push_back(nums2[j++]);
+
+        return ans;
+    }
+};
